c2b8b68ee689712de568b0636f3e2f61f91bcfe6
ax1lebafer <raul8917@gmail.com>
2025-06-11 18:02:39 +0400

    Footer

---
+++ src/components/Footer/constants.tsx
@@ -0,0 +1,14 @@
+import { ROUTES } from '@/routes';
+
+export const FOOTER_LINKS = [
+  { label: 'About bank', route: ROUTES.home },
+  { label: 'Ask a Question', route: ROUTES.home },
+  { label: 'Quality of service', route: ROUTES.home },
+  { label: 'Requisites', route: ROUTES.home },
+  { label: 'Press center', route: ROUTES.home },
+  { label: 'Bank career', route: ROUTES.home },
+  { label: 'Investors', route: ROUTES.home },
+  { label: 'Analytics', route: ROUTES.home },
+  { label: 'Business and processes', route: ROUTES.home },
+  { label: 'Compliance and business ethics', route: ROUTES.home },
+];
--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -2,28 +2,34 @@ import styles from './styles.module.scss';
 import Logo from '@/assets/images/logo.png';
 import { Link } from 'react-router-dom';
 import { ROUTES } from '@/routes';
+import { FOOTER_LINKS } from '@components/Footer/constants';
 
 export const Footer = () => {
   return (
     <footer className={styles.footer}>
       <div className={styles.footer__content}>
-        <div className={styles.footer__contacts}>
+        <section className={styles.footer__top}>
           <Link to={ROUTES.home}>
             <img className={styles.footer__logo} src={Logo} alt="Logo" />
           </Link>
 
-          <nav className={styles.footer__nav}>
-            <ul className={styles.footer__navlist}>
-              <li>
-                <a href="tel:+74959842513">+7 (495) 984 25 13</a>
-              </li>
+          <address className={styles.footer__contacts}>
+            <a href="tel:+74959842513">+7 (495) 984 25 13</a>
+            <a href="mailto:info@neoflex.ru">info@neoflex.ru</a>
+          </address>
+        </section>
 
-              <li>
-                <a href="mailto:info@neoflex.ru">info@neoflex.ru</a>
-              </li>
+        <section className={styles.footer__links}>
+          <nav className={styles.footer__nav}>
+            <ul className={styles.footer__navList}>
+              {FOOTER_LINKS.map((link) => (
+                <li key={link.label} className={styles.footer__navItem}>
+                  <Link to={link.route}>{link.label}</Link>
+                </li>
+              ))}
             </ul>
           </nav>
-        </div>
+        </section>
       </div>
     </footer>
   );
