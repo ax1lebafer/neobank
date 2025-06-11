08088bf299b67bea2a9cb88cb00abf3679346b63
ax1lebafer <raul8917@gmail.com>
2025-06-11 20:17:49 +0400

    Features component is done

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -1,9 +1,11 @@
 import { Greetings } from '@components/Home/Greetings';
+import { Features } from '@components/Home/Features';
 
 export const HomePage = () => {
   return (
     <>
       <Greetings />
+      <Features />
     </>
   );
 };
