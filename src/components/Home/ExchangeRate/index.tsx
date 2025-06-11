d6dd6cedc374df504f0d4af349456b5a4532d910
ax1lebafer <raul8917@gmail.com>
2025-06-11 20:43:16 +0400

    Start layout ExchangeCard component

---
+++ src/components/Home/ExchangeRate/index.tsx
@@ -0,0 +1,9 @@
+import styles from './styles.module.scss';
+
+export const ExchangeRate = () => {
+  return (
+    <section className={styles.exchange}>
+      <article className={styles.exchange__card}></article>
+    </section>
+  );
+};
--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -1,11 +1,13 @@
 import { Greetings } from '@components/Home/Greetings';
 import { Features } from '@components/Home/Features';
+import { ExchangeRate } from '@components/Home/ExchangeRate';
 
 export const HomePage = () => {
   return (
     <>
       <Greetings />
       <Features />
+      <ExchangeRate />
     </>
   );
 };
