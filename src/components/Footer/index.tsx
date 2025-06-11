0182c273c45f79517a826451f4d719241d8bf4ea
ax1lebafer <raul8917@gmail.com>
2025-06-11 14:26:01 +0400

    Footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,16 +1,27 @@
 import styles from './styles.module.scss';
 import Logo from '@/assets/images/logo.png';
+import { Link } from 'react-router-dom';
+import { ROUTES } from '@/routes';
 
 export const Footer = () => {
   return (
     <footer className={styles.footer}>
       <div className={styles.footer__content}>
         <div className={styles.footer__contacts}>
-          <img className={styles.footer__logo} src={Logo} alt="Logo" />
+          <Link to={ROUTES.home}>
+            <img className={styles.footer__logo} src={Logo} alt="Logo" />
+          </Link>
 
-          <nav>
-            <a href="tel:+74959842513">+7 (495) 984 25 13</a>
-            <a href="mailto:info@neoflex.ru">info@neoflex.ru</a>
+          <nav className={styles.footer__nav}>
+            <ul className={styles.footer__navlist}>
+              <li>
+                <a href="tel:+74959842513">+7 (495) 984 25 13</a>
+              </li>
+
+              <li>
+                <a href="mailto:info@neoflex.ru">info@neoflex.ru</a>
+              </li>
+            </ul>
           </nav>
         </div>
       </div>
