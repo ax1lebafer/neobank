1e7d0434b49f75f3572da0074cc86b5c5f4540af
ax1lebafer <raul8917@gmail.com>
2025-06-17 23:24:44 +0400

    Displaying currencies

--- src/components/Home/ExchangeRate/index.tsx
+++ src/components/Home/ExchangeRate/index.tsx
@@ -1,11 +1,47 @@
 import styles from './styles.module.scss';
 import { CurrentCurrency } from '@components/Home/ExchangeRate/CurrentCurrency';
-import { CURRENCIES } from '@components/Home/ExchangeRate/constants';
+import { DESIRED_CURRENCIES } from '@components/Home/ExchangeRate/constants';
 import { Link } from 'react-router-dom';
 import { ROUTES } from '@/routes';
 import BankImage from '@/assets/images/bank.svg';
+import { useAppDispatch, useAppSelector } from '@/store/store';
+import { getExchangeRateAsync } from '@/store/actions/ExchangeRate';
+import { useEffect, useState } from 'react';
 
 export const ExchangeRate = () => {
+  const dispatch = useAppDispatch();
+
+  const { loading, rate } = useAppSelector((state) => state.exchangeRate);
+
+  const { conversion_rates } = rate || {};
+
+  const [currencies, setCurrencies] = useState<
+    { currency: string; value: number }[]
+  >([]);
+
+  const handleGetExchangeRate = () => {
+    dispatch(getExchangeRateAsync({ base: 'RUB' }));
+  };
+
+  useEffect(() => {
+    handleGetExchangeRate();
+  }, []);
+
+  useEffect(() => {
+    if (!conversion_rates) return;
+
+    const filtered = DESIRED_CURRENCIES.map((currency) => ({
+      currency,
+      value: conversion_rates[currency],
+    }));
+
+    setCurrencies(filtered);
+  }, [conversion_rates]);
+
+  if (loading) {
+    return <p>Загрузка</p>;
+  }
+
   return (
     <section className={styles.exchange}>
       <article className={styles.exchange__card}>
@@ -15,7 +51,7 @@ export const ExchangeRate = () => {
           </h2>
           <p className={styles.exchange__subtitle}>Currency</p>
 
-          <CurrentCurrency currencies={CURRENCIES} />
+          <CurrentCurrency currencies={currencies} />
 
           <Link className={styles.exchange__link} to={ROUTES.home}>
             All courses
