e6897cb287c9d7e922e8c4d7b89a7d4fbd6d7780
ax1lebafer <raul8917@gmail.com>
2025-06-17 21:58:16 +0400

    Connected Redux Toolkit

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import App from '@/App';
+import { ReduxProvider } from '@/store/ReduxProvider';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <ReduxProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ReduxProvider>
   </StrictMode>
 );
