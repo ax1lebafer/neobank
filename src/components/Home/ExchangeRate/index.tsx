c3d5996f0ebadbd826fa9c5c38fd3dd4553b8f07
ax1lebafer <raul8917@gmail.com>
2025-06-18 15:54:33 +0400

    Set interval for updating rates

--- src/components/Home/ExchangeRate/index.tsx
+++ src/components/Home/ExchangeRate/index.tsx
@@ -6,7 +6,7 @@ import { ROUTES } from '@/routes';
 import BankImage from '@/assets/images/bank.svg';
 import { useAppDispatch, useAppSelector } from '@/store/store';
 import { getExchangeRateAsync } from '@/store/actions/ExchangeRate';
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 
 export const ExchangeRate = () => {
   const dispatch = useAppDispatch();
@@ -18,14 +18,38 @@ export const ExchangeRate = () => {
   const [currencies, setCurrencies] = useState<
     { currency: string; value: number }[]
   >([]);
+  const [lastUpdated, setLastUpdated] = useState<string>('');
 
-  const handleGetExchangeRate = () => {
-    dispatch(getExchangeRateAsync({ base: 'RUB' }));
-  };
+  const handleGetExchangeRate = useCallback(async () => {
+    try {
+      const data = await dispatch(
+        getExchangeRateAsync({ base: 'RUB' })
+      ).unwrap();
+
+      if (data.time_last_update_unix) {
+        const date = new Date(data.time_last_update_unix * 1000);
+        setLastUpdated(
+          date.toLocaleString('ru-RU', {
+            day: '2-digit',
+            month: '2-digit',
+            year: 'numeric',
+          })
+        );
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  }, [dispatch]);
 
   useEffect(() => {
     handleGetExchangeRate();
-  }, []);
+
+    const interval = setInterval(handleGetExchangeRate, 15 * 60 * 1000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, [handleGetExchangeRate]);
 
   useEffect(() => {
     if (!conversion_rates) return;
@@ -60,7 +84,7 @@ export const ExchangeRate = () => {
 
         <div className={styles.exchange__right}>
           <p className={styles.exchange__date}>
-            Update every 15 minutes, MSC 09.08.2022
+            Update every 15 minutes, MSC {lastUpdated}
           </p>
 
           <img className={styles.exchange__image} src={BankImage} alt="Bank" />
