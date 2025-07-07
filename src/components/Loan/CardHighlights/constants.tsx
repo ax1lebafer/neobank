142a2b815ca7474ee1e91c5f8ba7bff11420893c
ax1lebafer <raul8917@gmail.com>
2025-07-07 21:45:08 +0400

    Rates and conditions component

--- src/components/Loan/CardHighlights/constants.tsx
+++ src/components/Loan/CardHighlights/constants.tsx
@@ -1,9 +1,10 @@
 import { ITabItem } from '@components/shared/Tabs/types';
 import { AboutCard } from '@components/Loan/CardHighlights/AboutCard';
+import { Rates } from '@components/Loan/CardHighlights/Rates';
 
 export const CARD_TABS: ITabItem[] = [
   { id: 'about', label: 'About card', content: <AboutCard /> },
-  { id: 'rates', label: 'Rates and conditions', content: '2' },
+  { id: 'rates', label: 'Rates and conditions', content: <Rates /> },
   { id: 'cashback', label: 'Cashback', content: '3' },
   { id: 'faq', label: 'FAQ', content: '4' },
 ];
