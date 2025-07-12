cfd2db0d9b2f2144fd62db0b4cdf314332375b72
ax1lebafer <raul8917@gmail.com>
2025-07-13 00:50:28 +0400

    Adaptive layout

--- src/pages/LoanPage/index.tsx
+++ src/pages/LoanPage/index.tsx
@@ -10,7 +10,7 @@ export const LoanPage = () => {
   const scrollToForm = () => {
     prescoringRef.current?.scrollIntoView({
       behavior: 'smooth',
-      block: 'center',
+      block: 'start',
     });
   };
 
