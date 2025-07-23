7342d4d3aa1e58470d65142ecc8ef26640e9817b
ax1lebafer <raul8917@gmail.com>
2025-07-23 21:33:49 +0400

    Refactor custom button

--- src/components/ApplicationDocument/PaymentScheduleForm/index.tsx
+++ src/components/ApplicationDocument/PaymentScheduleForm/index.tsx
@@ -3,21 +3,34 @@ import { Table } from '@components/shared/Table';
 import { Checkbox } from '@components/UI/Checkbox';
 import { useAppSelector } from '@/store/store';
 import { DOCUMENT_COLUMNS } from '@components/ApplicationDocument/PaymentScheduleForm/constants';
+import { SkeletonBlock } from '@components/UI/SkeletonBlock';
+import { CustomButton } from '@components/UI/CustomButton';
 
 export const PaymentScheduleForm = () => {
-  const { applicationById } = useAppSelector((state) => state.loan);
+  const { applicationById, loading } = useAppSelector((state) => state.loan);
 
   const { credit } = applicationById ?? {};
 
   return (
-    <form className={styles.form}>
-      <div className={styles.form__header}>
-        <h2>Payment Schedule</h2>
-        <p className={styles.form__step}>Step 3 of 5</p>
-      </div>
-      <Table columns={DOCUMENT_COLUMNS} items={credit?.paymentSchedule || []} />
+    <SkeletonBlock
+      loading={loading}
+      skeletonProps={{ height: 500, borderRadius: 28 }}
+    >
+      <form className={styles.form}>
+        <div className={styles.form__header}>
+          <h2>Payment Schedule</h2>
+          <p className={styles.form__step}>Step 3 of 5</p>
+        </div>
+        <Table
+          columns={DOCUMENT_COLUMNS}
+          items={credit?.paymentSchedule || []}
+        />
 
-      <Checkbox label="I agree with the payment schedule" />
-    </form>
+        <div>
+          <CustomButton color="error">Deny</CustomButton>
+          <Checkbox label="I agree with the payment schedule" />
+        </div>
+      </form>
+    </SkeletonBlock>
   );
 };
--- src/components/UI/CustomButton/index.tsx
+++ src/components/UI/CustomButton/index.tsx
@@ -1,15 +1,26 @@
-import { ButtonHTMLAttributes, FC } from 'react';
+import { FC } from 'react';
 import styles from './styles.module.scss';
 import cn from 'classnames';
+import { ICustomButtonProps } from '@components/UI/CustomButton/types';
 
-export const CustomButton: FC<ButtonHTMLAttributes<HTMLButtonElement>> = ({
+export const CustomButton: FC<ICustomButtonProps> = ({
   children,
   type = 'button',
   className = '',
+  color = 'primary',
   ...rest
 }) => {
   return (
-    <button className={cn(styles.button, className)} type={type} {...rest}>
+    <button
+      className={cn(
+        styles.button,
+        color === 'primary' && styles.button__primary,
+        color === 'error' && styles.button__error,
+        className
+      )}
+      type={type}
+      {...rest}
+    >
       {children}
     </button>
   );
